0ecaae910efa306469fa55bae1fb1c13277954b6
Mozk Taberenai <mozk@me.com>
2023-09-23 15:23:20 +0900

    Update pool.rs
    fix error message

--- src/pool.rs
+++ src/pool.rs
@@ -40,7 +40,7 @@ pub(crate) enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::SlabTooLarge => write!(f, "slab must be less than {SLAB_SIZE}"),
+            Error::SlabTooLarge => write!(f, "Slab must be less than {SLAB_SIZE}"),
         }
     }
 }
